50871c1554d1e13aa48a47df6adb7b6e08966809
Ddhruv <54676859+Ddhruv-IOT@users.noreply.github.com>
2021-12-18 20:54:27 +0530

    basic logic gates implementation
    Implementation of AND OR NOT Logic Gates in Python

---
+++ basic_logic gates.py
@@ -0,0 +1,15 @@
+
+def or_gate(a, b):
+	if a == 1 or b == 1:
+		return 1
+	return 0 
+
+def and_gate(a, b): 
+	if a == 1 and b == 1:
+		return 1
+	return 0 
+
+def not_gate(a):
+	if a == 1:
+		return 0
+	return 1 
